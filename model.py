7996834fa2abedd19ce7c24e847c7d96b8cf1045
psyifa <155076789+psyifa@users.noreply.github.com>
2025-07-15 13:02:26 +0700

    Update model.py

--- model.py
+++ model.py
@@ -2,29 +2,34 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import joblib
 
 # Load the dataset
 url = 'https://raw.githubusercontent.com/psyifa/Aplikasi-Sains-Data/main/PreprocessingDatasetSephora.csv'
 data = pd.read_csv(url)
 
-# Split the dataset into training and testing sets
+# Split the dataset
 X = data['review_text']
 y = data['product_name']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create TF-IDF vectorizer
+# Vectorization
 vectorizer = TfidfVectorizer()
 X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
 
-# Train the model
+# Train model
 model = RandomForestClassifier()
 model.fit(X_train_tfidf, y_train)
 
-# Evaluate the model on the testing set
-X_test_tfidf = vectorizer.transform(X_test)
+# Evaluate
 accuracy = model.score(X_test_tfidf, y_test)
 print("Accuracy:", accuracy)
 
-# Save the trained model
-model.save("trained_model.pkl")
-vectorizer.save("vectorizer.pkl")
+# Save model & vectorizer
+joblib.dump(model, "trained_model.pkl")
+joblib.dump(vectorizer, "vectorizer.pkl")
+
+# Save accuracy to txt
+with open("akurasi_model.txt", "w") as f:
+    f.write(str(accuracy))
