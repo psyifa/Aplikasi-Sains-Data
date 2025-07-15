a81b78695568a5a56fbbcfb23876fd70ac5ab302
psyifa <155076789+psyifa@users.noreply.github.com>
2025-07-15 12:05:18 +0700

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -11,7 +11,20 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Pembersihan teks
 clean_spcl = re.compile('[/(){}\[\]\|@,;]')
 clean_symbol = re.compile('[^0-9a-z #+_]')
-stopworda = set(['were', 'should', "needn't", 'just', 'not', 's', "it's", 'between', 'are', 'all', 'did', 'until', 'will', 'your', 'against', 'needn', 'that', 'very', 'to', "shan't", 'in', 'doing', "hadn't", 'he', "couldn't", 'what', 'such', 'too', 'an', 'here', 'won', 'they', 've', "weren't", 'both', 'other', 'why', 'above', 'same', 'ma', 'ours', 'm', "shouldn't", 'haven', 'before', "wasn't", 'him', 'shan', 'yourselves', 'by', 'yours', 'his', 'the', 'because', 'most', "haven't", 'was', 'any', 'my', 'but', 'then', 'their', 'our', 'y', 'nor', 'shouldn', 'didn', "you'll", "didn't", 'of', 'isn', 'wouldn', 'theirs', 'them', 'few', 'there', 'd', 'further', 'o', 'myself', 'ain', 'at', 'now', 'be', "wouldn't", 'doesn', 'during', 'hers', "isn't", 'yourself', 'hadn', 'weren', 'some', "aren't", "she's", "that'll", 'over', 'she', 't', 'while', 'couldn', 'these', 'hasn', 'with', 'its', 'this', 'we', 'down', "mightn't", 'ourselves', 'me', 'once', 'how', 'about', 'through', 'and', "you're", 'll', 'as', "don't", 'am', 'having', 'whom', "you'd", 'for', 'have', 'can', 're', 'who', 'had', 'themselves', 'it', 'itself', 'again', 'does', 'i', 'into', 'if', 'her', "mustn't", "you've", "won't", 'those', 'on', 'or', 'under', 'only', 'so', 'mustn', 'a', 'don', 'being', 'is', 'where', 'herself', 'wasn', 'off', 'more', 'up', 'has', 'below', 'no', 'been', 'do', 'own', "should've", 'aren', "doesn't", "hasn't", 'than', 'which', 'each', 'from', 'you', 'when', 'after', 'out', 'himself', 'mightn'])
+stopworda = set(['were', 'should', "needn't", 'just', 'not', 's', "it's", 'between', 'are', 'all', 'did', 'until', 'will',
+'your', 'against', 'needn', 'that', 'very', 'to', "shan't", 'in', 'doing', "hadn't", 'he', "couldn't", 'what',
+'such', 'too', 'an', 'here', 'won', 'they', 've', "weren't", 'both', 'other', 'why', 'above', 'same', 'ma', 'ours',
+'m', "shouldn't", 'haven', 'before', "wasn't", 'him', 'shan', 'yourselves', 'by', 'yours', 'his', 'the', 'because',
+'most', "haven't", 'was', 'any', 'my', 'but', 'then', 'their', 'our', 'y', 'nor', 'shouldn', 'didn', "you'll",
+"didn't", 'of', 'isn', 'wouldn', 'theirs', 'them', 'few', 'there', 'd', 'further', 'o', 'myself', 'ain', 'at',
+'now', 'be', "wouldn't", 'doesn', 'during', 'hers', "isn't", 'yourself', 'hadn', 'weren', 'some', "aren't",
+"she's", "that'll", 'over', 'she', 't', 'while', 'couldn', 'these', 'hasn', 'with', 'its', 'this', 'we', 'down',
+"mightn't", 'ourselves', 'me', 'once', 'how', 'about', 'through', 'and', "you're", 'll', 'as', "don't", 'am',
+'having', 'whom', "you'd", 'for', 'have', 'can', 're', 'who', 'had', 'themselves', 'it', 'itself', 'again', 'does',
+'i', 'into', 'if', 'her', "mustn't", "you've", "won't", 'those', 'on', 'or', 'under', 'only', 'so', 'mustn', 'a',
+'don', 'being', 'is', 'where', 'herself', 'wasn', 'off', 'more', 'up', 'has', 'below', 'no', 'been', 'do', 'own',
+"should've", 'aren', "doesn't", "hasn't", 'than', 'which', 'each', 'from', 'you', 'when', 'after', 'out', 'himself',
+'mightn'])
 
 # Dataset
 url = 'https://raw.githubusercontent.com/psyifa/Aplikasi-Sains-Data/main/PreprocessingDatasetSephora.csv'
@@ -21,24 +34,20 @@ Data = pd.read_csv(url)
 title = "Beauty Things"
 subtitle = "Let's find skincare for you!"
 
-st.set_page_config(page_title = title,
-                   page_icon = None,
-                   layout = "wide")
-      
+st.set_page_config(page_title=title, page_icon=None, layout="wide")
 st.sidebar.header("Select the options :")
 
 with st.sidebar:          
-      Category = st.selectbox('Category', Data['secondary_category'].unique())
-      Skin = st.selectbox('Skin Tone', Data['skin_tone'].unique())
-      Type = st.selectbox('Skin Type', Data['skin_type'].unique())
+    Category = st.selectbox('Category', Data['secondary_category'].unique())
+    Skin = st.selectbox('Skin Tone', Data['skin_tone'].unique())
+    Type = st.selectbox('Skin Type', Data['skin_type'].unique())
       
 def clean_text(text): 
-    text = text.lower() # lowercase text
+    text = text.lower()
     text = clean_spcl.sub(' ', text)
     text = clean_symbol.sub('', text)
-    text = ' '.join(word for word in text.split() if word not in stopworda) # hapus stopword dari kolom deskripsi
+    text = ' '.join(word for word in text.split() if word not in stopworda)
     return text
-  
 
 def main():
     st.title("Beauty Things")
@@ -58,7 +67,7 @@ def main():
     plt.tight_layout()
     st.pyplot(fig)
     
-#     Filter Data
+    # Filter Data
     filtered_data = Data.loc[Data.secondary_category == Category]
     filtered_data = filtered_data.loc[filtered_data.skin_tone == Skin]
     filtered_data = filtered_data.loc[filtered_data.skin_type == Type]
@@ -67,34 +76,35 @@ def main():
     st.write(filtered_data[filtered_data.product_name == Product].review_text)
     st.write('Other Recommendations :')
     
+    # Preprocess
     Data['review_clean'] = Data['review_text'].apply(clean_text)
-
     Data.set_index('product_name', inplace=True)
     tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=1, stop_words='english')
     tfidf_matrix = tf.fit_transform(Data['review_clean'])
     cos_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
-    
     indices = pd.Series(Data.index)
     
-#     Rekomendasi
-    def recommendations(name, cos_sim = cos_sim):
+    # Rekomendasi
+    def recommendations(name, cos_sim=cos_sim):
         recommendedProducts = []
         recommendedProductsreview = []
-        idx =indices[indices == name].index[0]
-        score = pd.Series(cos_sim[idx]).sort_values(ascending = False)
+        idx = indices[indices == name].index[0]
+        score = pd.Series(cos_sim[idx]).sort_values(ascending=False)
         top_10 = list(score.iloc[1:11].index)
         for i in top_10:
             recommendedProducts.append(list(Data.index)[i])
             recommendedProductsreview.append(list(Data.review_text)[i])
         return recommendedProducts, recommendedProductsreview, score
-    
-    Rproduct, Rreview, score = recommendations(Product)
- 
-    Rprint = pd.DataFrame(({'Product': Rproduct,'Review': Rreview, 'Similarity Score':list(score.iloc[1:11])}))
-
-    st.table(Rprint)
-    
-            
+    if Product in Data.index:
+        Rproduct, Rreview, score = recommendations(Product)
+        Rprint = pd.DataFrame({
+            'Product': Rproduct,
+            'Review': Rreview,
+            'Similarity Score': list(score.iloc[1:11])
+        })
+        st.table(Rprint)
+    else:
+        st.info("Tidak ada rekomendasi yang tersedia untuk produk ini.")
 
 if __name__ == "__main__":
     main()
